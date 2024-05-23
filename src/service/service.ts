befbc8a543f5c899a65da1258276986ae56e8d01
bedirhan-1 <bedirhan9828@gmail.com>
2024-05-23 11:58:09 +0300

    refactor: refactor api

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,8 +1,13 @@
-import axios from './axios';
+import httpClient from './axios';
+import { CreatedAddress, IAddress } from '../types/addressTypes';
+import { Api } from '../types/apiTypes';
 
-const api = {
+const api: Api = {
 	address: {
-		getAll: () => axios.get('addressList'),
+		getAll: () => httpClient.get('addressList'),
+		add: (body: CreatedAddress) => httpClient.post('addressList', body),
+		update: (body: IAddress) => httpClient.put(`addressList/${body.id}`, body),
+		delete: (id: string) => httpClient.delete(`addressList/${id}`),
 	},
 };
 
