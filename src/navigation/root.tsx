1961531e219722f2f235e7cc8fc251e1fbc02970
bedirhan-1 <bedirhan9828@gmail.com>
2024-05-23 11:58:48 +0300

    refactor: review root navigation

--- src/navigation/root.tsx
+++ src/navigation/root.tsx
@@ -1,32 +1,42 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import { RootStackParamList, StackScreens } from './types';
+import { RootStackParamList, StackScreens } from '../types/navigationTypes';
 import { AddressList } from '../screens/AddressList';
 import { AddNewAddress } from '../screens/AddNewAddress';
 import { Header } from '../components/Navigator/Header';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Splash } from '../screens/Splash';
+import { NavigationContainer } from '@react-navigation/native';
 
 const RootNavigation = () => {
 	const Stack = createStackNavigator<RootStackParamList>();
 
 	return (
-		<SafeAreaProvider>
-			<Stack.Navigator
-				screenOptions={{
-					header: props => <Header {...props} />,
-				}}
-			>
-				<Stack.Screen
-					name={StackScreens.addressList}
-					component={AddressList}
-					options={{ title: 'Adres Bilgilerin', headerTitle: 'Adreslerim' }}
-				/>
-				<Stack.Screen
-					name={StackScreens.addNewAddress}
-					component={AddNewAddress}
-					options={{ title: 'Adres Bilgilerin', headerTitle: 'Adreslerim' }}
-				/>
-			</Stack.Navigator>
-		</SafeAreaProvider>
+		<NavigationContainer>
+			<SafeAreaProvider>
+				<Stack.Navigator
+					screenOptions={{
+						header: props => <Header {...props} />,
+					}}
+					initialRouteName={StackScreens.splash}
+				>
+					<Stack.Screen
+						name={StackScreens.splash}
+						component={Splash}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name={StackScreens.addressList}
+						component={AddressList}
+						options={{ title: 'Adres Bilgilerin', headerTitle: 'Adreslerim' }}
+					/>
+					<Stack.Screen
+						name={StackScreens.addNewAddress}
+						component={AddNewAddress}
+						options={{ title: 'Adres Bilgilerin', headerTitle: 'Adreslerim' }}
+					/>
+				</Stack.Navigator>
+			</SafeAreaProvider>
+		</NavigationContainer>
 	);
 };
 
