e9417dfed51e8dcd85fa96882f12cdd4a84e5c82
bedirhan-1 <bedirhan9828@gmail.com>
2024-05-25 00:49:43 +0300

    feat: change language button

--- src/components/Navigator/Header.tsx
+++ src/components/Navigator/Header.tsx
@@ -2,16 +2,40 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { StatusBar, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { ParamText } from '../Text';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTheme } from '../../hooks/useTheme';
 import { useNavigation } from '@react-navigation/native';
 import GoBack from '../../../assets/Icons/goBack';
+import Language from '../../../assets/Icons/language';
+import { setLanguage } from '../../core/i18next.config';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const LANGUAGE_PREFERENCE_KEY = 'LANGUAGE_PREFERENCE';
 
 export const Header = (route: any) => {
 	const { top } = useSafeAreaInsets();
 	const { goBack } = useNavigation();
 	const { TextTheme, ColorPallet } = useTheme();
 	const { options } = route;
+	const [currentLanguage, setCurrentLanguage] = useState<string>('en');
+
+	useEffect(() => {
+		const fetchLanguage = async () => {
+			const storedLanguage = await AsyncStorage.getItem(
+				LANGUAGE_PREFERENCE_KEY,
+			);
+			if (storedLanguage) {
+				setCurrentLanguage(storedLanguage);
+			}
+		};
+		fetchLanguage().then();
+	}, []);
+
+	const handleLanguageChange = async () => {
+		const newLanguage = currentLanguage === 'en' ? 'tr' : 'en';
+		await setLanguage(newLanguage);
+		setCurrentLanguage(newLanguage);
+	};
 
 	return (
 		<LinearGradient
@@ -22,13 +46,28 @@ export const Header = (route: any) => {
 			<View style={{ height: top * 0.5 }} />
 			<View style={[styles.titleContainer, styles.endTitle]}>
 				{route.progress.previous && (
-					<TouchableOpacity onPress={() => goBack()} style={styles.icon}>
+					<TouchableOpacity
+						onPress={() => goBack()}
+						style={[styles.icon, styles.arrow]}
+					>
 						<GoBack color={ColorPallet.grayscale.grey} />
 					</TouchableOpacity>
 				)}
 				<ParamText style={TextTheme.headerLabel} fontType={'regular16'}>
 					{options.headerTitle}
 				</ParamText>
+				<TouchableOpacity
+					onPress={handleLanguageChange}
+					style={[styles.icon, styles.changeLang]}
+				>
+					<Language fill={ColorPallet.grayscale.grey} height={30} width={30} />
+					<ParamText
+						style={{ color: ColorPallet.grayscale.grey }}
+						fontType={'medium16'}
+					>
+						{currentLanguage.toUpperCase()}
+					</ParamText>
+				</TouchableOpacity>
 			</View>
 			<View style={styles.endTitle}>
 				<ParamText style={[TextTheme.headerTitle]} fontType={'medium20'}>
@@ -55,6 +94,13 @@ const styles = StyleSheet.create({
 	icon: {
 		position: 'absolute',
 		padding: 12,
+	},
+	arrow: {
 		left: 0,
+	},
+	changeLang: {
+		right: 0,
+		flexDirection: 'row',
+		alignItems: 'center',
 	},
 });
