ad4fe3ec2e4842a89a668594d3f9231594f6adca
bedirhan-1 <bedirhan9828@gmail.com>
2024-05-24 17:01:56 +0300

    feat: update info bottom sheet for user actions

--- src/components/Sheet/InfoSheet.tsx
+++ src/components/Sheet/InfoSheet.tsx
@@ -1,23 +1,30 @@
 import SuccessCircleFill from '../../../assets/Icons/successCircleFill';
-import { ParamText } from '../../components/Text';
+import { ParamText } from '../Text';
 import { BottomSheetView } from '@gorhom/bottom-sheet';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useTheme } from '../../hooks/useTheme';
 import UpdateCircleFill from '../../../assets/Icons/updateCircleFill';
 import TrashIcon from '../../../assets/Icons/trashIcon';
+import { Button, ButtonTypes } from '../Button';
+import { StyleSheet, View } from 'react-native';
 
 export enum ContentType {
 	update = 'update',
 	create = 'create',
 	delete = 'delete',
+	alert = 'alert',
 }
 
 type SheetContentType = {
 	contentType: ContentType | undefined;
+	setAnswerYesOrNo: (answer: boolean) => void;
 };
 
-export const SheetContent: React.FC<SheetContentType> = ({ contentType }) => {
+export const InfoSheet: React.FC<SheetContentType> = ({
+	contentType,
+	setAnswerYesOrNo,
+}) => {
 	const { t } = useTranslation();
 	const { TextTheme } = useTheme();
 
@@ -29,6 +36,8 @@ export const SheetContent: React.FC<SheetContentType> = ({ contentType }) => {
 				return <UpdateCircleFill />;
 			case ContentType.delete:
 				return <TrashIcon />;
+			case ContentType.alert:
+				return null;
 		}
 	};
 
@@ -40,13 +49,44 @@ export const SheetContent: React.FC<SheetContentType> = ({ contentType }) => {
 				return t('Add-Address.update-success');
 			case ContentType.delete:
 				return t('Add-Address.delete-success');
+			case ContentType.alert:
+				return t('Add-Address.delete-are-you-sure');
 		}
 	};
 
 	return (
-		<BottomSheetView style={{ alignItems: 'center', paddingTop: 32 }}>
-			{renderIcon()}
+		<BottomSheetView style={{ paddingTop: 32 }}>
+			<View style={styles.icon}>{renderIcon()}</View>
 			<ParamText style={TextTheme.bottomSheet}>{renderText()}</ParamText>
+			{contentType === ContentType.alert && (
+				<View style={styles.buttonContainer}>
+					<Button
+						style={styles.button}
+						title={t('Global.yes')}
+						type={ButtonTypes.Secondary}
+						onPress={() => setAnswerYesOrNo(true)}
+					/>
+					<Button
+						style={[styles.button, { marginLeft: 10 }]}
+						title={t('Global.no')}
+						type={ButtonTypes.Primary}
+						onPress={() => setAnswerYesOrNo(false)}
+					/>
+				</View>
+			)}
 		</BottomSheetView>
 	);
 };
+
+const styles = StyleSheet.create({
+	buttonContainer: {
+		padding: 20,
+		flexDirection: 'row',
+	},
+	button: {
+		flex: 1,
+	},
+	icon: {
+		alignItems: 'center',
+	},
+});
