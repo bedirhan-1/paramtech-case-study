da59fad1270b3f9842512a790d63ae570243d584
bedirhan-1 <bedirhan9828@gmail.com>
2024-05-25 00:53:13 +0300

    feat: use async storage for localization

--- src/core/i18next.config.ts
+++ src/core/i18next.config.ts
@@ -1,6 +1,7 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import * as Localization from 'expo-localization';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { en, tr } from './localization';
 
 const resources = {
@@ -12,29 +13,40 @@ const resources = {
 	},
 };
 
-const getDeviceLanguage = () => {
+const LANGUAGE_PREFERENCE_KEY = 'LANGUAGE_PREFERENCE';
+
+const getDeviceLanguage = async () => {
+	const storedLanguage = await AsyncStorage.getItem(LANGUAGE_PREFERENCE_KEY);
+	if (storedLanguage) {
+		return storedLanguage;
+	}
 	const locales = Localization.getLocales();
+
 	if (locales && locales.length > 0) {
 		return locales[0].languageTag.split('-')[0];
 	}
 	return 'en';
 };
 
-i18next
-	.use(initReactI18next)
-	.init({
-		compatibilityJSON: 'v3',
-		debug: true,
-		lng: getDeviceLanguage(),
-		fallbackLng: 'en',
-		interpolation: {
-			escapeValue: false,
-		},
-		resources,
-	});
-
-const setLanguage = (languageTag: string) => {
-	i18next.changeLanguage(languageTag);
+const setLanguage = async (languageTag: string) => {
+	await AsyncStorage.setItem(LANGUAGE_PREFERENCE_KEY, languageTag);
+	await i18next.changeLanguage(languageTag);
 };
+
+getDeviceLanguage().then(lng => {
+	i18next
+		.use(initReactI18next)
+		.init({
+			compatibilityJSON: 'v3',
+			debug: true,
+			lng,
+			fallbackLng: 'en',
+			interpolation: {
+				escapeValue: false,
+			},
+			resources,
+		})
+		.then();
+});
 
 export { setLanguage };
